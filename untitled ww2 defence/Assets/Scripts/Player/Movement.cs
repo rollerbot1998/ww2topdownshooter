e401e703f98f6f0455997d80a6f6c01fbf452fea
SURFACE-PRO-2\Lachlan Johnson <lochiejohnson@y7mail.com>
2018-12-29 12:30:33 +1100

    added some basic sprites and objects

---
+++ untitled ww2 defence/Assets/Scripts/Player/Movement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void RotateLeft()
+    {
+        transform.Rotate(Vector3.forward * -90);
+    }
+
+    void RotateRight()
+    {
+        transform.Rotate(Vector3.forward * 90);
+    }
+}
